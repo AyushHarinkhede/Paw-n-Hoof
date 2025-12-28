361912bf7bc2472fe2851eb2020e0d379d3eb0d5
gauravdhande20 <gauravdhande2022004@gmail.com>
2025-12-28 18:04:39 +0530

    footer Done

--- Client/Client/src/Pages/Home.jsx
+++ Client/Client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Herosec from '../Components/Herosec.jsx'
+import Footer from '../Components/Footer.jsx'
 
 function Home() {
   return (
     <>
       <Herosec />
+      <Footer />
     </>
   )
 }
